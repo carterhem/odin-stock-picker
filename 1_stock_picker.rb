f1d12ae2b980212bb38a52d34ef9e5bd5e5a254e
Carter <carter.hemington@gmail.com>
2023-03-31 09:13:19 -0400

    complete stock picker

--- 1_stock_picker.rb
+++ 1_stock_picker.rb
@@ -2,23 +2,22 @@ def stock_picker(array)
   # find lowest day to buy that isn't in last array position
   # find biggest difference buy to sell
   # find highest price that isn't first day to sell
-  # gotta do differences as I have to buy before I can sell (maybe double loop again?)
-  p array
-  high_stock = 0
-  low_stock = 100
-  array.each do |stock|
-    if stock > high_stock # && stock position isn't one
-      high_stock = stock
-      high_stock
-    end
-      
-    if stock < low_stock # stock position isn't last
-      low_stock = stock
-      low_stock
+  
+  # p array
+  profit_margin = 0
+  days_to_sell = []
+
+  array.each_with_index do |stock1, index1|
+    array.each_with_index do |stock2, index2|
+      current_profit = stock2 - stock1
+      # puts "stock2 #{stock2} - stock1 #{stock1} = current_profit #{current_profit}"
+      if current_profit > profit_margin && index2 > index1
+        profit_margin = current_profit
+        days_to_sell = index1, index2
+      end
     end
   end
-  p "high_stock #{high_stock}"
-  p "low_stock #{low_stock}"
+  p days_to_sell
 end
 
 stock_picker([17,3,6,9,15,8,6,1,10])
